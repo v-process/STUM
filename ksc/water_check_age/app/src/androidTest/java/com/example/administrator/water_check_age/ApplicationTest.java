4978f85c664946f5a423253a8455f51a8e6ad72c
v-process <ksc3168@naver.com>
2015-03-27 16:38:27 +0900

    물의 양 알고리즘, 슬라이드 액티비티

---
+++ ksc/water_check_age/app/src/androidTest/java/com/example/administrator/water_check_age/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.administrator.water_check_age;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
