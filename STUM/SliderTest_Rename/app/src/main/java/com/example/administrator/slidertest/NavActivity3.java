00795c4bffc3b4210ccbeeb2df3002e3c70a4279
v-process <ksc3168@naver.com>
2015-04-23 14:23:53 +0900

    환경설정 구성, java와 fragment 이름 변경

---
+++ STUM/SliderTest_Rename/app/src/main/java/com/example/administrator/slidertest/NavActivity3.java
@@ -0,0 +1,46 @@
+package com.example.administrator.slidertest;
+
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+//네비게이션 프레그먼트3
+public class NavActivity3 extends NavBaseActivity {
+    private String[] navMenuTitles;
+    private TypedArray navMenuIcons;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav_activity3);
+
+        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items); // load titles from strings.xml
+
+        navMenuIcons = getResources().obtainTypedArray(R.array.nav_drawer_icons);//load icons from strings.xml
+
+        set(navMenuTitles,navMenuIcons);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_first, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ STUM/SliderTest_Rename/app/src/main/java/com/example/administrator/slidertest/ViewPagerFragment1.java
@@ -0,0 +1,35 @@
+package com.example.administrator.slidertest;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2015-03-25.
+ */
+
+public class ViewPagerFragment1 extends Fragment {
+
+    public static ViewPagerFragment1 newInstance() {
+        ViewPagerFragment1 fragment = new ViewPagerFragment1();
+        return fragment;
+    }
+
+
+    public ViewPagerFragment1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.viewpager_fragment1, container, false);
+
+        // Inflate the layout for this fragment
+        return v;
+    }
+}
