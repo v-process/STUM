e07e876fc63ad91aeedb152ca0e826b334286943
v-process <ksc3168@naver.com>
2015-05-11 02:20:56 +0900

    마신물과 현재 온도 가져오기

--- STUM/STUM/app/src/main/java/com/example/administrator/STUM/NavActivity1.java
+++ STUM/STUM/app/src/main/java/com/example/administrator/STUM/NavActivity1.java
@@ -6,16 +6,29 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 //네비게이션 프레그먼트3
 public class NavActivity1 extends NavBaseActivity {
     private String[] navMenuTitles;
     private TypedArray navMenuIcons;
-
+    TextView temp;
+    TextView drink;
+    ImageView image1;
+    ImageView image2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +37,9 @@ public class NavActivity1 extends NavBaseActivity {
         navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items); // load titles from strings.xml
         navMenuIcons = getResources().obtainTypedArray(R.array.nav_drawer_icons);//load icons from strings.xml
         set(navMenuTitles,navMenuIcons);
-
+        temp = (TextView)findViewById(R.id.temp_view);
+        drink = (TextView)findViewById(R.id.drink_view);
+        image1 = (ImageView)findViewById(R.id.char_1);
         //노티피케이션 버튼
         Button notificationButton = (Button) findViewById(R.id.notification_btn);
 
@@ -35,7 +50,50 @@ public class NavActivity1 extends NavBaseActivity {
                             "통지 메세지입니다.");
                 }
         });
+//현재 물의 온도 및 마신양 가져오는 함수 호출.
+        getimage();
+
     }
+//현재 물의 온도 및 마신양 가져오는 함수 정의.
+    void getimage(){
+        ParseUser user = ParseUser.getCurrentUser();
+        //서버에서 이미지 받아오는 곳.
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("ChartTest");
+        // query.whereEqualTo("User", user);
+        // query.orderByAscending("createdAt");
+
+        query.addDescendingOrder("createdAt");
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            public void done(ParseObject object, ParseException e) {
+                if (object == null) {
+                    Log.d("score", "The getFirst request failed.");
+                    Toast.makeText(getBaseContext(), "온도와 마신물 없나바", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Log.d("score", "Retrieved the object.");
+                    Toast.makeText(getBaseContext(), "물양이랑 온도 있나바 : ", Toast.LENGTH_SHORT).show();
+
+                    int temp_1;
+                    int drink_1;
+                    temp_1 = (int) object.get("drink");
+                    drink_1 = (int) object.get("temp");
+
+                    charimage(drink_1);
+
+                    temp.setText(String.valueOf(temp_1));
+                    drink.setText(String.valueOf(drink_1));
+                }
+            }
+        });
+    }
+
+//이미지 세팅 함수.
+    void charimage(int drink_1){
+        if (drink_1 < 50){
+            image1.setImageResource(R.drawable.screaming_android);
+        }
+    }
+
 
     @SuppressWarnings("deprecation")
     private void Notify(String notificationTitle, String notificationMessage) {
